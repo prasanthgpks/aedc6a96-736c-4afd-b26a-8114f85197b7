4e9b1631721e95ce4999d45b2c876648c06249a5
prasanthgpks <prasanthgpks@gmail.com>
2022-05-02 23:36:28 +1000

    commit CovertStringToListOfNumbers method

--- LongestIncreasingSequence/LIS.Core/LISFunctions.cs
+++ LongestIncreasingSequence/LIS.Core/LISFunctions.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LIS.Core
 {
     public class LISFunctions
     {
+        /// <summary>
+        /// Returns a list of numbers from a given string
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public static IList<int> CovertStringToListOfNumbers(string inputString)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(inputString)) throw new ArgumentException("Parameter not supplied");
 
+                var resultList = new List<int>();
+                var stringList = inputString.Split(' ').ToList();                
+                stringList.ForEach(element => resultList.Add(int.Parse(element)));
+
+                return resultList;
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException($"Failed to process the provided input : {inputString}, please check your input and try again");
+            }
+        }
     }
 }
